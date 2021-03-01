2428e8fe43010a6bdd8eebce699a3007f438d13c
Birdgeek <birdgeekthree@gmail.com>
2021-03-01 11:09:00 -0700

    VMCSDDC v1.1

--- Get-VMCORG.ps1
+++
@@ -1,8 +0,0 @@
-﻿$apiToken = Read-Host -Prompt 'Enter you CSP API Token'
-$server = Connect-Vmc -ApiToken $apiToken
-$awsAcc = Get-AwsAccount -Server $server
-$vmcDX = Get-VmcSddc -Name 'CSE-DX'
-Write-Host ''
-Write-Host Successfully connected with $server.User  defaulting to ORG ID  $awsAcc
-Write-Host ''
-Write-Host $vmcDX.Id / $vmcDX.Name - $vmcDX.Region
